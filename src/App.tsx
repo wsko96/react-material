48719b92a179b46077ebf30e130d922e1db82ac4
Woonsan Ko <wsko@wise.co.kr>
2021-08-03 09:31:12 +0900

    post lab01

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Button from '@material-ui/core/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" color="primary">
+          시작이 반이다
+        </Button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
